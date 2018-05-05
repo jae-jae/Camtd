6e1db6b4c80e5bc887ac1c0bf5e7d1725935e333
Jaeger <JaegerCode@gmail.com>
2018-05-05 13:46:03 +0800

    fixed bug

--- app/scripts.babel/background.js
+++ app/scripts.babel/background.js
@@ -174,18 +174,17 @@ function rightadd(info, tab) {
   var down = { filename: '' };
   down.finalUrl = info.linkUrl;
   down.referrer = info.pageUrl;
-  if (!path || !size) {
+  if (!path) {
     alert('Camtd has not been configured');
     chrome.tabs.create({ 'url': 'options.html' }, function (s) { });
     return 0;
   }
-  getUrlCookie(down.url, (cookies) => {
+  getUrlCookie(down.finalUrl, (cookies) => {
     var aria2_obj = combination(down,cookies);
     var ifpostback = postaria2obj(aria2_obj);
     if (ifpostback == 'base64_error') {
       notice('Error adding tasks to aria2!','Error')
     } else {
-      chrome.downloads.cancel(down.id, function (s) { });
       // notice('Aria2 is starting to download files.', down.filename)
       sendAnimMsg()
     }
